dc1a5a51d75a8bc89f7e4e54d08bd3bc4978c4ca
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-10-21 23:41:59 +0530

    add new project PDFTextBot

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -7,16 +7,20 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
    
     {
-        id: 1,
-        name: 'JobLynk',
-        description: 'JobLynk is a comprehensive MERN stack job portal application designed to connect students with potential job opportunities. It allows job seekers to create profiles, browse job listings, and apply for positions, while enabling companies to post job vacancies, manage applications, and find suitable candidates efficiently. The platform features a responsive design for optimal user experience across devices and implements secure user authentication.',
-        tools: ['React', 'Vite', 'TailwindCSS', 'Node.js', 'Express.js', 'MongoDB', 'JWT', 'Multer', 'Redux Toolkit', 'Axios', 'Radix UI'],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: '',
-        link: 'https://joblynk-frontend.onrender.com/',
-        projectgithub: 'https://github.com/sanjeevpatel3007/jobLynk',
+
+      id: 1,
+      name: 'PDFTextBOT: AI-Integrated Full Stack Project',
+      description: "I developed an AI-powered chatbot that processes both PDF and text inputs to generate relevant and accurate responses. The backend was built using Node.js, and the front end is powered by Next.js with Tailwind CSS for responsive design. The chatbot integrates AI models via the OpenAI API to enhance user input and provide smarter responses. The project is deployed on Vercel for high availability.",
+      tools: ['Next.js', 'Node.js', 'OpenAI API', 'Tailwind CSS', 'Vercel'],
+      role: 'Full Stack Developer',
+      code: '',
+      demo: '',
+      image: '',
+      link: 'https://pdf-text-bot.vercel.app/',
+      projectgithub: ' https://github.com/sanjeevpatel3007/PDFTextBot'
+
+
+       
       },
       {
         id: 2,
@@ -31,7 +35,25 @@ export const projectsData = [
         projectgithub: 'https://github.com/sanjeevpatel3007/dotblog',
       },
       {
-        id: 3, 
+
+        id: 3,
+        name: 'JobLynk',
+        description: 'JobLynk is a comprehensive MERN stack job portal application designed to connect students with potential job opportunities. It allows job seekers to create profiles, browse job listings, and apply for positions, while enabling companies to post job vacancies, manage applications, and find suitable candidates efficiently. The platform features a responsive design for optimal user experience across devices and implements secure user authentication.',
+        tools: ['React', 'Vite', 'TailwindCSS', 'Node.js', 'Express.js', 'MongoDB', 'JWT', 'Multer', 'Redux Toolkit', 'Axios', 'Radix UI'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        image: '',
+        link: 'https://joblynk-frontend.onrender.com/',
+        projectgithub: 'https://github.com/sanjeevpatel3007/jobLynk',
+
+
+      
+      }
+      ,
+    
+      {
+        id: 4, 
         name: 'Wanderlust - Airbnb Clone',
         description: 'Wanderlust is a full-stack web application that replicates the core functionalities of Airbnb. The application allows users to browse listings, make bookings, and manage their rental properties. It is built using EJS templates for dynamic server-side rendering and Express.js for handling the backend logic. The app provides a seamless experience for users looking for short-term rentals, with features like filtering, property management, and secure authentication.',
         tools: ['EJS', 'Express.js', 'Node.js', 'MongoDB', 'Bootstrap', 'Passport.js'],
@@ -41,20 +63,6 @@ export const projectsData = [
         image: '', 
         link: 'https://wanderlust-gwow.onrender.com/listings', 
         projectgithub: 'https://github.com/sanjeevpatel3007/wanderLust', 
-      }
-      ,
-    {
-        id: 4,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
-        code: '',
-        demo: '',
-        image: crefin,
-        link:'https://www.msn.com/en-in',
-        projectgithub:'https://github.com/sanjeevpatel3007/jobLynk',
-
     },
   
    
