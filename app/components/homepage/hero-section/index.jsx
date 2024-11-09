cd3aa30551f14e29d0832929bf838ad21aaf3797
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-11-09 16:51:27 +0530

    update text in hero section

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -23,12 +23,15 @@ function HeroSection() {
 
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-        <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-  Hello, <br />
-  I'm <span className="text-pink-500">{personalData.name}</span>,<br />
-  a Professional <span className="text-[#16f2b3]">{personalData.designation}</span>.
-</h1>
-
+          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+            Hello, <br />
+             {` I'm `}
+            <span className=" text-pink-500">{personalData.name}</span>
+            <br />
+            {` a Professional `}
+            <span className=" text-[#16f2b3]">{personalData.designation}</span>
+            .
+          </h1>
 
           <div className="my-12 flex items-center gap-5">
            
